39211fa9714588f4d4060df1fddf60294b7baa0a
Bill Dunay <bdunay3@comcast.net>
2023-07-03 11:59:08 -0400

    Bumped Package to Swift 5.9, added some docs about the host site

--- Sources/PokeApi/PokeApiClient/PokeApiClient.swift
+++ Sources/PokeApi/PokeApiClient/PokeApiClient.swift
@@ -1,6 +1,12 @@
 import Combine
 import Foundation
 
+// Sets up URLSession to connect to the PokeAPI.
+//
+// Additional Information:
+// * Fully open source
+// * The API is currently hosted at: https://pokeapi.co
+// * If you want to spin up your own instance and not hit the public host you can grab the source at: https://github.com/PokeAPI/pokeapi/
 public final class PokeApiClient {
     public static var hostUrl: URL = {
         guard let hostUrl = URL(string: "https://pokeapi.co/api/v2") else {
